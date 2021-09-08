539032169f9d197103610896cfccba7ea383ae39
shakti3103 <sakthiravi0799@gmail.com>
2021-09-08 09:48:10 +0530

    Username Functionality

--- src/org/resources/BaseClass.java
+++ src/org/resources/BaseClass.java
@@ -3,7 +3,7 @@ package org.resources;
 public class BaseClass {
 	 
 	public static void  username() {
-		System.out.println("username");
+		System.out.println("password");
 	}
 	public static void main(String[] args) {
 		username();
