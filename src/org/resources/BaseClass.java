a101d8ef37f0eac504ae72f33a31161877a027a6
shakti3103 <sakthiravi0799@gmail.com>
2021-09-08 10:45:19 +0530

    Functionality

--- src/org/resources/BaseClass.java
+++ src/org/resources/BaseClass.java
@@ -1,5 +1,11 @@
 package org.resources;
 
 public class BaseClass {
-
+	 
+	public static void  username() {
+		System.out.println("username");
+	}
+	public static void main(String[] args) {
+		username();
+	}
 }
